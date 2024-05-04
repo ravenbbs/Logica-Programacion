d45604759287d8ae8f5630acd8daf325210b59d4
ravenbbs <julioc3bgud@gmail.com>
2024-05-03 20:15:53 -0500

    Ejercicio resuelto para python

---
+++ 0 - Sintaxis, variables, tipos de datos y hola mundo/Python/python.py
@@ -0,0 +1,35 @@
+#https://www.python.org/
+
+#Comentario de una linea
+
+"""
+Comentario de 
+varias 
+lineas
+"""
+
+#En python se usa snakecase, todo minúscula y cada palabra se separa por un guion bajo.
+
+mi_variable = 12
+mi_string_variable = "Strig"
+
+#En python no existe las constantes, se utiliza una variable con mayusculas y separado por un guion bajo. 
+MY_CONSTANTE = "STRING CONSTANTE" 
+
+#Tipos de datos primitivos 
+entero = 12
+flotante = 12.5
+boolean = True
+boolean = False
+cadena_de_texto = "Cadena de texto"
+otra_cadena_de_texto = 'Otra de texto'
+
+#Imprimir por consola
+print("Hola mundo en Python :D")
+
+#Verificar los tipos de datos
+print(type(entero))
+print(type(flotante))
+print(type(boolean))
+print(type(cadena_de_texto))
+print(type(otra_cadena_de_texto))
